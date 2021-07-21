849d77e73a8281b0548b64afb0014a3965c59cc2
Guido <gsalcedo@MacBook-Pro-de-Guido.local>
2021-07-21 20:17:20 -0300

    Se modifica el index de clients

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,22 +1,15 @@
 import { IOClients } from '@vtex/api'
 
-import Status from './status'
-import Pricing from './pricing'
-
-import { CatalogGraphQLClient } from './Catalog/GraphQLClient'
-
+import Inventory from './inventory'
+import InventoryExternal from './inventoryExternal'
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
-  public get status() {
-    return this.getOrSet('status', Status)
-  }
-  
-  public get pricing() {
-    return this.getOrSet('pricing', Pricing)
+  public get inventory() {
+    return this.getOrSet('inventory', Inventory)
   }
 
-  public get catalogGQL(): CatalogGraphQLClient {
-    return this.getOrSet('CatalogGraphQL', CatalogGraphQLClient)
+  public get inventoryExternal() {
+    return this.getOrSet('inventoryExternal', InventoryExternal)
   }
 }
