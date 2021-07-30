8cf01aecbf49dcf0b7ec7feeb73b802de607eb0c
Guido <gsalcedo@MacBook-Pro-de-Guido.local>
2021-07-30 15:28:18 -0300

    The route and the verb of the service is changed

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -3,7 +3,6 @@ import { IOClients } from '@vtex/api'
 import InventoryGraphQL from './inventoryGraphQL'
 import InventoryRestClient from './inventoryRestClient'
 
-// Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
   public get inventoryGraphQL() {
     return this.getOrSet('inventoryGraphQL', InventoryGraphQL)
